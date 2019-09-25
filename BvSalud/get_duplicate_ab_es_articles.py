4c5b17a6b6d364e49272c0756c296d9f194c3f61
Ankush Rana <ankush.ran13@gmail.com>
2019-09-25 11:18:34 +0200

    Finished with get all_duplicate....

--- BvSalud/get_duplicate_ab_es_articles.py
+++ BvSalud/get_duplicate_ab_es_articles.py
@@ -2,7 +2,8 @@
 from bvs.constant import DATA_BASE,COLLECTION_ALL,COLLECTIONS_NONE_INDEXED_T1,COLLECTION_UPDATE_INFO
 from pymongo import MongoClient
 import json
-import os
+import os, csv
+
 
 
 
@@ -11,12 +12,37 @@ db = client[DATA_BASE]
 collection_all = db[COLLECTION_ALL]
 collection_duplicate_ab_es = db["duplicate_abstracts"]
 
-def find_articles():
+def find_articles(o_path):
     duplicate_articles = collection_duplicate_ab_es.find()
     print(collection_duplicate_ab_es.count())
+    
+    csv_columns = ["matching_code","id","title","abstract","article_date","entry_date","journal","mh","sh"]
+    csv_file = open(o_path, 'w')
+    writer = csv.DictWriter(csv_file, delimiter='|',fieldnames=csv_columns)
+    writer.writeheader()
 
-    for abstract in duplicate_articles:
-        print(abstract)
+    for i, abstract in enumerate(duplicate_articles):
+        for id in abstract["Ids"]:
+            print(i)
+            article = collection_all.find_one({"_id":id})
+            article_dict = {"matching_code":i,
+                "id": article["_id"],
+                "title": article["ti_es"],
+                "abstract" :article["ab_es"],
+                "article_date" : article["da"],
+                "entry_date" : article["entry_date"],
+                "journal" : article["cc"],
+                "mh" :article["mh"],
+                "sh" : article["sh"]}
+            writer.writerow(article_dict)
+        
+    csv_file.close()
 
-
-find_articles()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog ='get_duplicate_ab_es_articles.py',usage='%(prog)s[-o file.csv]')
+    parser.add_argument('-o','--output',metavar='',type=str,required=True, help ='To define a name for file.')   
+    args = parser.parse_args()
+    output = args.output
+    current_dir = os.getcwd()
+    path = os.path.join(current_dir,output)
+    find_articles("pr.csv")
