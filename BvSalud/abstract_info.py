6ca4f43463c345e555e8d4cc895a4b9bdaf42080
Ankush Rana <ankush.ran13@gmail.com>
2019-10-04 11:40:35 +0200

    abstract_info

---
+++ BvSalud/abstract_info.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from bvs.constant import DATA_BASE,COLLECTION_ALL,COLLECTIONS_NONE_INDEXED_T1
+from langdetect import detect
+from pymongo import MongoClient
+
+
+cTraining = "training" #Condition to select if training. 
+cGold = "gold" #Condition for gold Set
+client = MongoClient('localhost:27017')
+db = client[DATA_BASE] #DATA_BASE is a constant, please check bvs/constant.py for all constants.
+collection_all = db[COLLECTION_ALL] # Also a constant COLLECTION_ALL
+collection_None_Indexed_t1 =db[COLLECTIONS_NONE_INDEXED_T1] # Also a constant COLLECTION_NONE_INDEXED_T1
+
+
+def main():
+    cursor_mongo = collection_all.find({"$and":[
+            {"ab_es":{"$ne": None}},
+            {"mh":{"$ne":None}}]})
+
+
+    error_file = open("abstract_lang_error.txt","w")
+
+
+    for i, document_dict in enumerate(cursor_mongo):
+        print(i)
+        try:
+            ab_language = detect(document_dict["ab_es"]) # trying to detect the language, if can't it will return false and print a massage.
+            if ab_language != 'es': # If the language is spanish it will return a true, else it will return false and print a error massage. 
+                error_file.write(str(ab_language+ "\t"+ document_dict["ab_es"] + "\n"))
+        except:
+            error_file.write(str("null\t"+ document_dict["ab_es"] + "\n"))
+
+main()
+       
+  
