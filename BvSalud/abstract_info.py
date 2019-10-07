c823714adafdc29ed78d590da75ef69af4a951ed
Ankush Rana <ankush.ran13@gmail.com>
2019-10-07 09:46:23 +0200

    abstract.py argparse

--- BvSalud/abstract_info.py
+++ BvSalud/abstract_info.py
@@ -2,6 +2,8 @@
 from bvs.constant import DATA_BASE,COLLECTION_ALL,COLLECTIONS_NONE_INDEXED_T1
 from langdetect import detect
 from pymongo import MongoClient
+import argparse
+import os
 
 
 cTraining = "training" #Condition to select if training. 
@@ -12,7 +14,7 @@ collection_all = db[COLLECTION_ALL] # Also a constant COLLECTION_ALL
 collection_None_Indexed_t1 =db[COLLECTIONS_NONE_INDEXED_T1] # Also a constant COLLECTION_NONE_INDEXED_T1
 
 
-def main():
+def main(path_output):
     
     cursor_mongo = collection_all.find({"$and":[
             {"ab_es":{"$nin":[None,"No disponible","No disponble","No dispoinble","No disponbile",
@@ -22,7 +24,7 @@ def main():
 
     len_cursor = cursor_mongo.count()
     print("Total Doc:",len_cursor)
-    error_file = open("abstract_lang_error.txt","w")
+    error_file = open(path_output,"w")
 
     for i, document_dict in enumerate(cursor_mongo):
         print(len_cursor-i)
@@ -33,6 +35,16 @@ def main():
         except:
             error_file.write(str(document_dict["_id"] + "\tnull\t"+ document_dict["ab_es"] + "\n"))
 
-main()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog ='abstract_info.py',usage='%(prog)s [-o file.json]')
+    parser.add_argument('-o','--output',metavar='',type=str,required=True, help ='To define a name for file.')  
+
+    args = parser.parse_args()
+    output = args.output
+
+    current_dir = os.getcwd()
+    path_output = os.path.join(current_dir,output)
+    main(path_output)
        
   
